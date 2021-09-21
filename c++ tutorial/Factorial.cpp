0b1d5859e4f28bf2927c4efc61a87b11327bc686
samiyadev <91077255+samiyadev@users.noreply.github.com>
2021-09-21 11:11:45 +0530

    Add files via upload
    c++ notes

---
+++ c++ tutorial/Factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+   int i,fact=1,number;    
+  cout<<"Enter any Number: ";    
+ cin>>number;    
+  for(i=0;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  cout<<"Factorial of " <<number<<" is: "; 
+  cout<<fcat<<endl; 
+  return 0;  
+}  
